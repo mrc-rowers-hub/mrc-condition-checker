8343db9b58e26ec3c3c8b77499dd64cbc244f49c
AdelaideBaron <baronadelaide@hotmail.com>
2024-06-09 22:55:15 +0100

    spring project

---
+++ condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/ConditionCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mersey.rowing.club.condition_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
