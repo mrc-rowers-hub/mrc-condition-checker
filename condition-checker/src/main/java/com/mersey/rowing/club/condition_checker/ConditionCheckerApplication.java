888a70cc5f76fb1395f03a6be353d363f6093809
AdelaideBaron <baronadelaide@hotmail.com>
2024-06-09 22:44:51 +0100

    spring boot project

---
+++ condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/ConditionCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.mersey.rowing.club.condition_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionCheckerApplication.class, args);
+	}
+
+}
