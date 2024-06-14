9da58d888d903707c60232a89a22d508720c06d4
AdelaideBaron <baronadelaide@hotmail.com>
2024-06-14 22:04:31 +0100

    open weather response models

---
+++ condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/mock/openweatherapi/OpenWeatherResponse.java
@@ -0,0 +1,20 @@
+package com.mersey.rowing.club.condition_checker.mock.openweatherapi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.util.List;
+
+@Builder
+@Value
+@AllArgsConstructor
+@Jacksonized
+public class OpenWeatherResponse {
+
+    @JsonProperty("data")
+    private List<WeatherData> data;
+
+}
---
+++ condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/mock/openweatherapi/Weather.java
@@ -0,0 +1,27 @@
+package com.mersey.rowing.club.condition_checker.mock.openweatherapi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+@Builder
+@Value
+@AllArgsConstructor
+@Jacksonized
+public class Weather {
+
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("main")
+    private String main;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("icon")
+    private String icon;
+
+}
