976aeb4a8e12f9726f99ba75d3dc9f7bb3d6f749
AdelaideBaron <baronadelaide@hotmail.com>
2024-06-14 20:26:08 +0100

    changing mock app to be java 17

--- open-weather-mock/README.md
+++ open-weather-mock/README.md
@@ -1,4 +1,4 @@
-# Open-weather-api mock 
+# Open-weather-api mock
 
 ## How to run
 Prerequisites:
@@ -8,3 +8,6 @@ Instructions:
 - Run `docker-compose -f dockercompose.yaml logs` (this starts the wiremock container)
 
 Wiremock is used for mocking the open weather api. We currently have no contract tests in place for this, and manually check this. There is a ticket in place for this. 
+
+## Version 
+WireMock is supported for Java 11 or 17. So, as this is merely to setup the mocks, we have chosen to keep this app as Java 17. 
---
+++ open-weather-mock/src/main/java/com/mersey/rowing/club/open_weather_mock/mock/WireMockConfiguration.java
@@ -0,0 +1,30 @@
+package com.mersey.rowing.club.open_weather_mock.mock;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WireMockConfiguration {
+    private final String host;
+    private final int port;
+
+    public WireMockConfiguration(
+            @Value("${wiremock.host}") String host, @Value("${wiremock.port}") int port) {
+        this.host = host; // these values are set in the (test) app.yml
+        this.port = port;
+    }
+
+    /**
+     * Create a WireMock instance that connects to our standalone WireMock server. We need to specify
+     * destroyMethod = "", so that Spring will not call WireMock.shutdown() (which is inferred) which
+     * would lead to the standalone server being closed after every test run.
+     *
+     * @return A WireMock instance pointing at our standalone.
+     */
+    @Bean(destroyMethod = "")
+    public WireMock wireMock() {
+        return new WireMock(host, port);
+    }
+}
