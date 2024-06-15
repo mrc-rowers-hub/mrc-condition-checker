cb1dcf198ad038f594680b080863159ff3a456f7
AdelaideBaron <baronadelaide@hotmail.com>
2024-06-15 20:51:21 +0100

    reset tests

--- condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/ConditionCheckerApplicationTests.java
+++ condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/ConditionCheckerApplicationTests.java
@@ -1,8 +1,6 @@
 package com.mersey.rowing.club.condition_checker;
 
-import com.mersey.rowing.club.condition_checker.controller.boats.BoatCapabilityClient;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,8 +8,6 @@ class ConditionCheckerApplicationTests {
 
 	@Test
 	void contextLoads() {
-		BoatCapabilityClient boatCapabilityClient = new BoatCapabilityClient();
-//		boatCapabilityClient.getBoatConfig();
 	}
 
 }
