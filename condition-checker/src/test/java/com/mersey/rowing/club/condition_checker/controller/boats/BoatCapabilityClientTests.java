03d0765aa26b0a2f7e3247d38fe0bf2b37343c9e
AdelaideBaron <baronadelaide@hotmail.com>
2024-06-15 22:31:42 +0100

    start on tests

--- condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/controller/boats/BoatCapabilityClientTests.java
+++ condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/controller/boats/BoatCapabilityClientTests.java
@@ -1,18 +1,41 @@
 package com.mersey.rowing.club.condition_checker.controller.boats;
 
+import com.mersey.rowing.club.condition_checker.mock.MockOpenWeatherResponseGenerator;
+import com.mersey.rowing.club.condition_checker.model.openweatherapi.OpenWeatherResponse;
+import com.mersey.rowing.club.condition_checker.model.response.BoatsAllowed;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 public class BoatCapabilityClientTests {
 
     @Autowired
     private BoatCapabilityClient boatCapabilityClient;
 
+    private final MockOpenWeatherResponseGenerator oWResponseGenerator = new MockOpenWeatherResponseGenerator();
+
+    private BoatsAllowed.BoatsAllowedBuilder boatsAllowedBuilder;
+
+    @BeforeEach
+    public void setUp() {
+        boatsAllowedBuilder = BoatsAllowed.builder();
+    }
+
 
     @Test
     void contextLoads() {
-        boatCapabilityClient.getBoatLimit();
+//        log.info(String.valueOf(boatCapabilityClient.getBoatsAllowed(oWResponseGenerator.getOpenWeatherResponseGenericFeelsLike(274))));
     }
+
+
+    // scenarios
+    // temp above, below, and within range
+    // wind speed above limit, and within
+    // weather code acceptable, exlusion, non-acceptable
+    // variety of wind speeds
 }
