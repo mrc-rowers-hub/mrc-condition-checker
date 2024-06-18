de46f423355cf9bd0084824439a7e4e008d1d953
AdelaideBaron <baronadelaide@hotmail.com>
2024-06-18 20:12:48 +0100

    reset controller

--- condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/controller/ConditionController.java
+++ condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/controller/ConditionController.java
@@ -3,48 +3,28 @@ package com.mersey.rowing.club.condition_checker.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 @RestController
 @Slf4j
 public class ConditionController {
 
-    // Date in format YYYY-MM-dd, time in format HH:mm
     @GetMapping("/get_conditions")
-    public void getConditions(@RequestParam(value = "date", required = false) String date, @RequestParam(value = "time", required = false) String time){
-        log.info("date supplied {}, time supplied {}", date, time);
-    }
-
-    private LocalDateTime getDateTimeToSearch(String date, String time){
-        LocalDateTime suppliedLocalDateTime = LocalDateTime.now();
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public void getConditions(@RequestHeader(value = "date", required = false) String date, @RequestHeader(value = "time", required = false) String time){
 
         if(date == null && time == null){
-            log.info("date and time is null, default to now");
-            return suppliedLocalDateTime;
+            log.info("date and time is null");
+            // go with default of today + 24 hours
         } else if (date == null) {
-            log.info("date only is null, default to today");
-            LocalTime localTime = LocalTime.parse(time, timeFormatter);
-            return LocalDateTime.of(suppliedLocalDateTime.toLocalDate(), localTime);
+            log.info("date only is null");
         } else if (time == null) {
-            log.info("time only is null, default to 6am"); // consider if this is done for today,... only do 6am if in the future, otherwise do now perhaps?
-            LocalDate localDate = LocalDate.parse(date, dateFormatter);
-            time = "06:00";
-            LocalTime localTime = LocalTime.parse(time, timeFormatter);
-            return LocalDateTime.of(localDate, localTime);
+            log.info("time only is null");
+            // will return a response of weather summary, and boats that can go out
+            // call open weather API and map the response
+            // pass this into the BoatCapabilityClient to assess which boats can go
         }else{
             log.info("both date and time supplied");
-            LocalDate localDate = LocalDate.parse(date, dateFormatter);
-            LocalTime localTime = LocalTime.parse(time, timeFormatter);
-            return LocalDateTime.of(localDate, localTime);
+            //specific
         }
     }
-
 }
