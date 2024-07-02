48da6d06ac5d61412e0a1551863a7223cd9949d0
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-02 14:27:56 +0100

    Test context

---
+++ condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/Context.java
@@ -0,0 +1,9 @@
+package com.mersey.rowing.club.condition_checker;
+
+public class Context { // Todo rename
+
+    public static final String BASE_URL = "http://localhost:5050";
+    public static final String DUMMY_API_KEY = "testApiKey";
+    public static final String TEST_EPOCH_TIME = "1625088000";
+
+}
