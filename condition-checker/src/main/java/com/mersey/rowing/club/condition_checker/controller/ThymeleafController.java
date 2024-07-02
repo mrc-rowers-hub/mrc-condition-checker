947145fa1036ca5fd6d8d67f3f6df6688d7f6eae
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-02 16:19:36 +0100

    adding current date & time

--- condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/controller/ThymeleafController.java
+++ condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/controller/ThymeleafController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class ThymeleafController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("name", "World");
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Europe/London"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+
+        model.addAttribute("currentDateTime", formattedDateTime);
         return "index";
     }
 }
