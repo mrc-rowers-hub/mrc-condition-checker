93e28edb637734c24b6580053b67c72cc14882e1
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-02 15:40:31 +0100

    refactoring

--- condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/applicationTests/WiremockBaseTests.java
+++ condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/applicationTests/WiremockBaseTests.java
@@ -5,12 +5,11 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.mersey.rowing.club.condition_checker.mockSetup.WireMockConfiguration;
 import com.mersey.rowing.club.condition_checker.mockSetup.WireMockSetup;
 import com.mersey.rowing.club.condition_checker.model.openweatherapi.OpenWeatherResponse;
+import com.mersey.rowing.club.condition_checker.utils.TestOpenWeatherUtils;
 import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
@@ -19,41 +18,27 @@ import org.springframework.context.annotation.Import;
 @Slf4j
 public class WiremockBaseTests {
 
-    @Autowired
-    private WireMock wireMock;
-
-    @Autowired
-    private WireMockSetup wireMockSetup;
-
-    @Value("${open-weather-api.endpoint}")
-    private String endpoint;
-
-    private static final String BASE_URL = "http://localhost:5050";
-    private static final String DUMMY_API_KEY = "testApiKey";
-    private static final String TEST_EPOCH_TIME = "1625088000";
-
-    protected static OpenWeatherResponse expectedOpenWeatherResponse;
-
+    protected static OpenWeatherResponse expectedGenericOpenWeatherResponse;
     protected static String url;
     protected static String expectedResponseBody;
 
     @BeforeAll
     static void setUpBeforeClass() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-        RestAssured.baseURI = BASE_URL;
+        RestAssured.baseURI = TestOpenWeatherUtils.BASE_URL;
 
-        url = BASE_URL + "/timemachine?lat=53.39293&lon=-2.98532&dt=" + TEST_EPOCH_TIME + "&appid=" + DUMMY_API_KEY;
+        url = TestOpenWeatherUtils.BASE_URL + "/timemachine?lat=53.39293&lon=-2.98532&dt=" + TestOpenWeatherUtils.TEST_EPOCH_TIME + "&appid=" + TestOpenWeatherUtils.DUMMY_API_KEY;
 
         log.info("<<<<< SETTING UP WIREMOCK MAPPINGS >>>>>");
         WireMockSetup mockSetup = new WireMockSetup();
         MappingBuilder mappingBuilder = mockSetup.setupOpenWeatherMapping();
-        expectedResponseBody = mockSetup.getOpenWeatherResponseAsString();
+        expectedResponseBody = TestOpenWeatherUtils.getOpenWeatherResponseAsString();
 
         WireMock wireMock = new WireMock("localhost", 5050);
         wireMock.register(mappingBuilder);
         log.info("<<<<< WIREMOCK MAPPINGS COMPLETE SETUP >>>>>");
 
-        expectedOpenWeatherResponse = mockSetup.getOpenWeatherResponse();
+        expectedGenericOpenWeatherResponse = TestOpenWeatherUtils.getOpenWeatherResponse();
     }
 
     @AfterAll
--- condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/mockSetup/WireMockSetup.java
+++ condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/mockSetup/WireMockSetup.java
@@ -2,16 +2,9 @@ package com.mersey.rowing.club.condition_checker.mockSetup;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.github.tomakehurst.wiremock.client.MappingBuilder;
-import com.mersey.rowing.club.condition_checker.model.openweatherapi.OpenWeatherResponse;
-import com.mersey.rowing.club.condition_checker.model.openweatherapi.Weather;
-import com.mersey.rowing.club.condition_checker.model.openweatherapi.WeatherData;
+import com.mersey.rowing.club.condition_checker.utils.TestOpenWeatherUtils;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 @Slf4j
 public class WireMockSetup {
@@ -19,27 +12,8 @@ public class WireMockSetup {
     private final String endpoint = "/timemachine";
 
     public MappingBuilder setupOpenWeatherMapping() {
-        String body = getOpenWeatherResponseAsString();
-
+        String body = TestOpenWeatherUtils.getOpenWeatherResponseAsString();
         return get(urlPathMatching(endpoint)).willReturn(aResponse().withStatus(200).withBody(body).withHeader("Content-Type", "application/json"));
-    }
-
-    public String getOpenWeatherResponseAsString(){
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        try{
-            return ow.writeValueAsString(getOpenWeatherResponse());
-        } catch(JsonProcessingException e){
-            log.error("Error while parsing JSON", e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    // move the below to testUtils
-    public OpenWeatherResponse getOpenWeatherResponse(){
-        List<Weather> weatherList = List.of(
-                new Weather(800, "clear sky", "01d"));
-        List<WeatherData> weatherDataList = List.of(new WeatherData(279.13, 276.44, 3.6, weatherList));
-        return OpenWeatherResponse.builder().data(weatherDataList).build();
     }
 
 }
