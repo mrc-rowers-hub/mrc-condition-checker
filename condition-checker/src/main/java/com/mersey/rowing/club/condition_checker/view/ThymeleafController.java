5729d8f4e9c71abcd03f73b05f3a97bd4ff740e0
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-21 18:49:17 +0100

    adding date specific page

--- condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/view/ThymeleafController.java
+++ condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/view/ThymeleafController.java
@@ -3,6 +3,7 @@ package com.mersey.rowing.club.condition_checker.view;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ThymeleafController {
@@ -15,5 +16,11 @@ public class ThymeleafController {
     model.addAttribute("nextDays", WebsiteDateUtils.getNext4Days());
 
     return "index";
+  }
+
+  @GetMapping("/date")
+  public String dateDetails(@RequestParam("selectedDate") String selectedDate, Model model) {
+    model.addAttribute("selectedDate", selectedDate);
+    return "dateDetails";
   }
 }
