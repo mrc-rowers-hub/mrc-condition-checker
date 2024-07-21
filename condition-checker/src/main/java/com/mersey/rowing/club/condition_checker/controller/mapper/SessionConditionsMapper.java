f57b65cb7310b52fdcba2f09ad26347303b1fcbe
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-21 16:48:51 +0100

    mapping for unhappy response from OW

--- condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/controller/mapper/SessionConditionsMapper.java
+++ condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/controller/mapper/SessionConditionsMapper.java
@@ -2,11 +2,13 @@ package com.mersey.rowing.club.condition_checker.controller.mapper;
 
 import com.mersey.rowing.club.condition_checker.controller.boats.BoatCapabilityClient;
 import com.mersey.rowing.club.condition_checker.controller.util.DateUtil;
+import com.mersey.rowing.club.condition_checker.model.StatusCodeObject;
 import com.mersey.rowing.club.condition_checker.model.openweatherapi.OpenWeatherResponse;
 import com.mersey.rowing.club.condition_checker.model.response.BoatsAllowed;
 import com.mersey.rowing.club.condition_checker.model.response.SessionConditions;
 import com.mersey.rowing.club.condition_checker.model.response.WeatherConditions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,7 +18,7 @@ public class SessionConditionsMapper {
 
   private DateUtil dateUtil = new DateUtil();
 
-  public SessionConditions mapFromOwResponse(OpenWeatherResponse openWeatherResponse) {
+  public SessionConditions mapFromHappyOwResponse(OpenWeatherResponse openWeatherResponse) {
     BoatsAllowed boatsAllowed = boatCapabilityClient.getBoatsAllowed(openWeatherResponse);
 
     WeatherConditions weatherConditions =
@@ -35,5 +37,14 @@ public class SessionConditionsMapper {
         .boatsAllowed(boatsAllowed)
         .date(dateTime)
         .build();
+  }
+
+  public SessionConditions mapFromUnhappyOwResponse(StatusCodeObject statusCodeObject){
+    if(statusCodeObject.getHttpStatus().equals(HttpStatus.OK)){
+      throw new RuntimeException("Cannot map a valid response to unhappy response");
+    }
+    String status = statusCodeObject.getHttpStatus().toString();
+    String date = (String) statusCodeObject.getOwResponse();
+    return SessionConditions.builder().status(status).date(date).build();
   }
 }
--- condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/applicationTests/controller/mapper/SessionConditionsMapperTests.java
+++ condition-checker/src/test/java/com/mersey/rowing/club/condition_checker/applicationTests/controller/mapper/SessionConditionsMapperTests.java
@@ -5,14 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mersey.rowing.club.condition_checker.controller.mapper.SessionConditionsMapper;
 import com.mersey.rowing.club.condition_checker.mockSetup.MockOpenWeatherResponseGenerator;
+import com.mersey.rowing.club.condition_checker.model.StatusCodeObject;
 import com.mersey.rowing.club.condition_checker.model.openweatherapi.OpenWeatherResponse;
 import com.mersey.rowing.club.condition_checker.model.response.SessionConditions;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
 @Slf4j
 @SpringBootTest(properties = {"open-weather-api.key=test"})
@@ -21,6 +25,9 @@ public class SessionConditionsMapperTests {
 
   private static OpenWeatherResponse MOCK_OW_RESPONSE =
       MockOpenWeatherResponseGenerator.getOpenWeatherResponseAllGood();
+
+  private     ObjectMapper mapper = new ObjectMapper();
+;
 
   private static String EXPECTED_RESPONSE =
       """
@@ -38,15 +45,17 @@ public class SessionConditionsMapperTests {
             }}
             """;
 
+  @BeforeEach
+  public void init(){
+    mapper.registerModule(new JavaTimeModule());
+  }
+
   @Test
   void mapSessionConditionsFromOpenWeatherResponse_validOpenWeatherResponse_mapsAsExpected() {
-    SessionConditions sessionConditions =
-        sessionConditionsMapper.mapFromOwResponse(MOCK_OW_RESPONSE);
-
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.registerModule(new JavaTimeModule());
+    SessionConditions actualSessionConditions =
+        sessionConditionsMapper.mapFromHappyOwResponse(MOCK_OW_RESPONSE);
     try {
-      String sessionConditionsJson = mapper.writeValueAsString(sessionConditions);
+      String sessionConditionsJson = mapper.writeValueAsString(actualSessionConditions);
       JSONAssert.assertEquals(EXPECTED_RESPONSE, sessionConditionsJson, false);
     } catch (AssertionError e) {
       log.error("JSON objects are not equal: " + e.getMessage());
@@ -55,4 +64,31 @@ public class SessionConditionsMapperTests {
       throw new RuntimeException(e);
     }
   }
+
+  @Test
+  void mapFromUnhappyOwResponse_200Response_throwsException(){
+    StatusCodeObject statusCodeObject = new StatusCodeObject(HttpStatus.OK, MOCK_OW_RESPONSE);
+    Assertions.assertThrows(RuntimeException.class, () -> {
+      sessionConditionsMapper.mapFromUnhappyOwResponse(statusCodeObject);
+    });
+  }
+
+  @Test
+  void mapFromUnhappyOwResponse_401Response_mapsToSessionResponse(){
+    StatusCodeObject statusCodeObject = new StatusCodeObject(HttpStatus.UNAUTHORIZED, "17/06/2024 20:46");
+    SessionConditions actualSessionConditions = sessionConditionsMapper.mapFromUnhappyOwResponse(statusCodeObject);
+    SessionConditions expectedSessionConditionsResponse = SessionConditions.builder().status(HttpStatus.UNAUTHORIZED.toString()).date("17/06/2024 20:46").build();
+    try {
+      String sessionConditionsJson = mapper.writeValueAsString(actualSessionConditions);
+      String expectedSessionConditionsJson = mapper.writeValueAsString(expectedSessionConditionsResponse);
+      JSONAssert.assertEquals(expectedSessionConditionsJson, sessionConditionsJson, false);
+    } catch (AssertionError e) {
+      log.error("JSON objects are not equal: " + e.getMessage());
+      throw e;
+    } catch (JSONException | JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
 }
