2d681a26353625ba09f2731b9eb34aa572540de8
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-23 17:07:54 +0100

    wip getting average

--- condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/view/ThymeleafController.java
+++ condition-checker/src/main/java/com/mersey/rowing/club/condition_checker/view/ThymeleafController.java
@@ -1,5 +1,6 @@
 package com.mersey.rowing.club.condition_checker.view;
 
+import com.mersey.rowing.club.condition_checker.controller.boats.BoatCapabilityClient;
 import com.mersey.rowing.club.condition_checker.controller.response.ConditionResponseClient;
 import com.mersey.rowing.club.condition_checker.controller.util.DateUtil;
 import com.mersey.rowing.club.condition_checker.model.response.ConditionResponse;
@@ -21,6 +22,9 @@ public class ThymeleafController {
   @Autowired ConditionResponseClient conditionResponseClient;
 
   @Autowired DateUtil dateUtil;
+
+  @Autowired
+  BoatCapabilityClient boatCapabilityClient;
 
   @GetMapping("/")
   public String index(Model model) {
@@ -44,11 +48,17 @@ public class ThymeleafController {
     List<SessionConditions> conditionResponseOfStartTimes = conditionResponse.getSessionConditions().stream().filter(sessionConditions -> sessionConditions.getTimeType().equals(TimeType.SESSION_START)).toList();
 
     for(SessionConditions sessionConditions : conditionResponseOfStartTimes){
+      // EACH SESSION - HAVE THIS IN A BIG BOX
       String uuid = sessionConditions.getSessionUUID();
       List<SessionConditions> conditionsDuringSession = conditionResponse.getSessionConditions().stream().filter(sessionConditions1 -> sessionConditions1.getSessionUUID().equals(uuid)).toList();
-    }
+      // for the whole session, have one big 'boats permitted' thing:
+      boatCapabilityClient.getSessionAverage(conditionsDuringSession); // I want this display in big - do not change this call
+    } // want to map all of these like they are now, but in the same box - e.g. all sessions in one big box, then in their separate boxes
 
+    // then remove the below
     model.addAttribute("sessionConditions", conditionResponseOfStartTimes);
+
+    // and have in
 
     return "dateDetails";
   }
